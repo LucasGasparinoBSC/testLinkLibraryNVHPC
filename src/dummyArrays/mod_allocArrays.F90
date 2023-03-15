b586f9fe6c4252e3ac91c6acdd1d78fde95600d8
Lucas Gaparino <lucas.gasparino@bsc.es>
2023-03-15 12:36:15 +0100

    The bug persists even if the array is made PROTECTED or PRIVATE. Adding the subroutine that allocates it changes nothiing, the issue is within the array declaration itself.

--- src/dummyArrays/mod_allocArrays.F90
+++ src/dummyArrays/mod_allocArrays.F90
@@ -1,5 +1,13 @@
 module mod_allocArrays
     implicit none
     integer(4), parameter :: nArr = 100
-    real(4), allocatable :: arr1(:)
+    real(4), private, allocatable :: arr1(:)
+    contains
+#if 0
+        subroutine allocArrays()
+            implicit none
+            integer(4) :: i
+            allocate(arr1(nArr))
+        end subroutine allocArrays
+#endif
 end module mod_allocArrays
