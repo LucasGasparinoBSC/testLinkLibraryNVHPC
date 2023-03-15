77eb1cdec1fe4703915c40eb0dcc874af4b85b93
Lucas Gaparino <lucas.gasparino@bsc.es>
2023-03-15 12:03:55 +0100

    By removing the "parameter" attribute, now everythingg works fine.

--- src/dummyArrays/mod_paramArrays.F90
+++ src/dummyArrays/mod_paramArrays.F90
@@ -1,5 +1,5 @@
 module mod_paramArrays
     implicit none
     integer(4), parameter :: dummy1 = 1
-    integer(4), parameter :: dummy2(3) = 1
+    integer(4) :: dummy2(3) = 1
 end module mod_paramArrays
